72e75dfe00412ce57a9730634ab37f24b0564eb8
William St-Arnaud <william.starnaud@gmail.com>
2019-02-06 22:04:53 -0500

    Added evaluation of the network

--- eval.py
+++ eval.py
@@ -8,7 +8,8 @@ import os
 
 
 parser = argparse.ArgumentParser(description="Script to evaluate a convolutional network")
-parser.add_argument("--path", default=util.cwd)
+parser.add_argument("--path", default=os.path.join(util.cwd, "model.pt"))
+parser.add_argument("--data", default=os.path.join(util.cwd, "mnist.npy"))
 
 
 def load_model(conv_net, path):
@@ -24,11 +25,38 @@ def load_model(conv_net, path):
     return conv_net
 
 
+def eval(conv_net, test):
+    """
+    Function that evaluates the convolutional network on the test data
+    :param conv_net:
+    :param test:
+    :return:
+    """
+    data, label = test
+    data = np.reshape(data, [-1, 1, 28, 28])
+
+    data, label = torch.from_numpy(data), torch.from_numpy(label)
+
+    loss_op = torch.nn.CrossEntropyLoss()
+
+    output = conv_net(data)
+    loss = loss_op(output, label).item()
+    acc = torch.eq(output.argmax(dim=1), label).float().mean().item()
+
+    print("The test loss is: ", loss, "\t", "The test accuracy is: ", acc)
+
+
 if "__main__" == __name__:
     namespace = parser.parse_args()
+
+    # load the test data
+    _, _, test = util.load_data(namespace.data)
 
     # Create convolutional network
     conv_net = ConvNet()
 
     # load the network for evaluation
     conv_net = load_model(conv_net, namespace.path)
+
+    # evaluate the network
+    eval(conv_net, test)
--- util.py
+++ util.py
@@ -5,12 +5,12 @@ import numpy as np
 cwd = os.path.dirname(os.path.realpath(__file__))
 
 
-def load_data(path=cwd):
+def load_data(path=os.path.join(cwd, "mnist.py")):
     """
     Function to load the data
     :param path: Path from where we load the data
     :return: The training set, validation set and test set
     """
-    train, valid, test = np.load(os.path.join(path, "mnist.npy"))
+    train, valid, test = np.load(path)
 
     return train, valid, test
